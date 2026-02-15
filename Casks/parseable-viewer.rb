1d84e05810229d296190f8802a18a7a568b7c4fc
iko <mail@iko.soy>
2026-02-16 01:26:01 +0300

    Update parseable-viewer.rb

--- Casks/parseable-viewer.rb
+++ Casks/parseable-viewer.rb
@@ -1,10 +1,10 @@
 cask "parseable-viewer" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "7eebda4"
-  sha256 arm: "bd7d8e5baaf21d8d12c443fb7ddc127506f856bcf577c7a99d53bbc9933f35d4", intel: "c07c7b1dbe01d31ac4c95b25a308015505e97c212b4a01e54fc31b19f25d3015"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/ilyakooo0/parseable-macos/releases/download/latest/ParseableViewer-#{arch}.zip"
+  url "https://github.com/ilyakooo0/parseable-macos/releases/latest/download/ParseableViewer-#{arch}.zip"
   name "Parseable Viewer"
   desc "A macOS client for Parseable"
   homepage "https://github.com/ilyakooo0/parseable-macos"
